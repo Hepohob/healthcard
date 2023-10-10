7a7914d3bcbe575d819d188d1fa37f008782c091
Aleksei Neronov <aleksei.neronov@adviqo.com>
2023-10-10 16:47:47 +0200

    Initial Commit

---
+++ Test NFC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test NFC
+//
+//  Created by Aleksei Neronov on 10.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
